bfe13b328a2c9900b44c4d293863e67206a2755b
haipenge@gmail.com <haipenge@gmail.com>
2017-02-22 14:23:35 +0800

    修改依赖文件

---
+++ faceye-search-web/src/test/java/com/faceye/test/component/search/repository/ArticleCategoryRepositoryTestCase.java
@@ -0,0 +1,22 @@
+package com.faceye.test.component.search.repository;
+
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.eclipse.core.runtime.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.faceye.component.search.doc.ArticleCategory;
+import com.faceye.component.search.repository.mongo.ArticleCategoryRepository;
+import com.faceye.test.feature.repository.BaseRepositoryTestCase;
+
+public class ArticleCategoryRepositoryTestCase extends BaseRepositoryTestCase {
+	@Autowired
+	ArticleCategoryRepository articleCategoryRepository =null;
+	@Test
+	public void testGetAll() throws Exception{
+		List<ArticleCategory> categories=this.articleCategoryRepository.findAll();
+		Assert.isTrue(CollectionUtils.isNotEmpty(categories));
+	}
+}
--- faceye-search-web/start.sh
+++ faceye-search-web/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git pull
+#git pull
 mvn clean compile jetty:run -D jetty.port=8094 -D maven.test.skip=true -P test
