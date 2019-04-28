5e04a8fe4c9c95333fd0efd6d5c2558d8aaccd3e
haipenge@gmail.com <haipenge@gmail.com>
2019-04-28 16:33:31 +0800

    change mongo version

--- faceye-search-manager/src/test/java/com/faceye/test/component/parse/service/SuperParseServiceTestCase.java
+++ faceye-search-manager/src/test/java/com/faceye/test/component/parse/service/SuperParseServiceTestCase.java
@@ -5,10 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import com.faceye.component.parse.service.ParseService;
+import com.faceye.component.spider.doc.CrawlResult;
+import com.faceye.component.spider.service.CrawlResultService;
 import com.faceye.test.feature.service.BaseServiceTestCase;
 
 public class SuperParseServiceTestCase extends BaseServiceTestCase {
 
+	@Autowired
+	private CrawlResultService crawlResultService=null;
 	@Autowired
 	@Qualifier("superParseServiceImpl")
 	private ParseService parseService=null;
@@ -16,5 +20,11 @@ public class SuperParseServiceTestCase extends BaseServiceTestCase {
 	@Test
 	public void testParse() throws Exception{
 		this.parseService.saveParseResult();
+	}
+	
+	@Test
+	public void testPageParse() throws Exception{
+		CrawlResult crawlResult=this.crawlResultService.get(1327L);
+		this.parseService.saveParseResult(crawlResult);
 	}
 }
