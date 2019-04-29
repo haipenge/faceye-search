036e28b86f8cd098e0b186b3840cd38a0df75826
haipenge@gmail.com <haipenge@gmail.com>
2019-04-29 09:24:11 +0800

    kill jetty

--- faceye-search-manager/start-jetty.sh
+++ faceye-search-manager/start-jetty.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-echo `ps aux|grep jetty |grep -v grep  | awk '{print $2}'|xargs kill -9`
-build_env = 'product'
+ROOT=$(dirname $(cd "$(dirname "$0")";pwd))
+echo $ROOT
+cd $ROOT/faceye-search-manager
+build_env='product'
 if [[ -n $1 ]];then
 	build_env=$1
 fi
-mvn clean compile jetty:run -D maven.test.skip=true -D jetty.port=8081 -P $build_env &
+_count=`ps -ef | grep jetty | grep -v "grep" | wc -l`
+echo 'Jetty Process count is:'$_count
+echo 'Now,Build Env is:',$build_env
+echo 'Jetty is killed,rebuild project and start jetty now'
+mvn clean compile jetty:run -D maven.test.skip=true -D jetty.port=8081 -P $build_env
