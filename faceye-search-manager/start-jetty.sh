ae7da730be8367c65525a5d21b7eb0bb833db6b0
haipenge@gmail.com <haipenge@gmail.com>
2019-04-29 09:38:44 +0800

    add jetty:stop configurations

--- faceye-search-manager/start-jetty.sh
+++ faceye-search-manager/start-jetty.sh
@@ -2,12 +2,11 @@
 ROOT=$(dirname $(cd "$(dirname "$0")";pwd))
 echo $ROOT
 cd $ROOT/faceye-search-manager
+mvn jetty:stop
 build_env='product'
 if [[ -n $1 ]];then
 	build_env=$1
 fi
-_count=`ps -ef | grep jetty | grep -v "grep" | wc -l`
-echo 'Jetty Process count is:'$_count
 echo 'Now,Build Env is:',$build_env
-echo 'Jetty is killed,rebuild project and start jetty now'
+git pull
 mvn clean compile jetty:run -D maven.test.skip=true -D jetty.port=8081 -P $build_env
