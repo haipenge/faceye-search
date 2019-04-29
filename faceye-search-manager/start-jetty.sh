05c41d8246910fd56507b8870b6783f0b5031ed5
haipenge@gmail.com <haipenge@gmail.com>
2019-04-29 08:51:23 +0800

    add env to start-jetty.sh

--- faceye-search-manager/start-jetty.sh
+++ faceye-search-manager/start-jetty.sh
@@ -1,3 +1,7 @@
 #Run faceye-search-mamager with jetty,because Weixin security in resin is unabled.
 echo `ps aux|grep jetty |grep -v grep  | awk '{print $2}'|xargs kill -9`
-mvn clean compile jetty:run -D maven.test.skip=true -D jetty.port=8081 -P product &
+build_env = 'product'
+if [[ -n $1 ]];then
+	build_env=$1
+fi
+mvn clean compile jetty:run -D maven.test.skip=true -D jetty.port=8081 -P $build_env &
